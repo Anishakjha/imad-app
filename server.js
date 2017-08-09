3ba2b9f4f9a63ec40ef27139026d65a5e99b4226
Anishakjha <30622288+Anishakjha@users.noreply.github.com>
2017-08-09 15:33:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,11 +144,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req,res) {
-res.send(createTemplate(article-one[articles]));
+res.send(createTemplate(article-one));
 });
 
 app.get('/article-two', function (req,res) {
-res.send(createTemplate(article-two[articles]));
+res.send(createTemplate(article-two));
 });
 
 app.get('/ui/style.css', function (req, res) {

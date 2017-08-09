af501ca901cb208c32f826e831e184fe31a8fec1
Anishakjha <30622288+Anishakjha@users.noreply.github.com>
2017-08-09 11:04:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne= {
+    title: 'Article One-Anisha',
+    heading:'First Article',
+    content: `<p>
+        One of the hardest things about improving your life is remembering to practice what you've learned in a moment of temptation, frustration, or hardship. Anyone can follow a strategy as they read about it, but remembering to stick with it in the real world is tough.
+    </p>
+    <p>
+          Stories help with that. An engaging story sticks with you in a way that a research finding often can't. While JamesClear.com promotes science-backed ideas, we don't shun stories and lessons based on real life.
+    </p>`
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+var htmlTemplate = `
+<html>
+    
+    <head>
+    
+    <title>
+        ${title}
+    </title>
+    <meta name ="viewport" content = "width-device-width,initial-scale = 1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+
+    
+    </head>
+     
+     <body>
+    <div class = "Style">
+    <div>
+       <a href = "/">Home Page</a>
+    </div>
+    <hr/>
+    
+    <h1>
+        ${heading}
+    </h1>
+    
+    <div>
+        Date
+    </div>
+    
+   ${content}
+   </div>
+   </body>
+
+   </html>
+   ';
+   
+    return htmlTemplate ;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/Article-one', function (req,res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});    
+ res.send(createTemplate(articleOne));
+});
 
 app.get('/Article-two', function (req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));

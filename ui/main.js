1ed668e5fc3a8fdce67a0b35a6ca673fb3a15c1b
Anishakjha <30622288+Anishakjha@users.noreply.github.com>
2017-08-09 19:21:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'This is the modified version of my web app' ;
+
+var img = document.getElementById('girl');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};

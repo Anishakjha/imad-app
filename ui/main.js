b1a147afc4ec88ad2fd70684358b88da2506a0d3
Anishakjha <30622288+Anishakjha@users.noreply.github.com>
2017-08-09 19:32:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,16 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'This is the modified version of my web app' ;
 
 var img = document.getElementById('girl');
+
+
+var marginRight = 0 ; 
+function moveLeft() {
+marginRight = marginRight + 10;
+img.style.marginRight = marginRight + 'px';
+    
+}
+
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveLeft, 100);
+    
 };

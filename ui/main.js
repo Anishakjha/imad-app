ed0275851e5b3a7729b8a6670355e0a4c94e0c15
Anishakjha <30622288+Anishakjha@users.noreply.github.com>
2017-08-11 21:12:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,9 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'This is the modified version of my web app' ;
+var button = document.getElementById('counter');
+var counter = 0;
 
-var img = document.getElementById('girl');
-
-
-var marginRight = 0 ; 
-function moveLeft() {
-marginRight = marginRight + 10;
-img.style.marginRight = marginRight + 'px';
-    
-}
-
-img.onclick = function() {
-    var interval = setInterval(moveLeft, 100);
-    
+button.onclick = function() {
+  
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
